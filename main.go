5710e46900975c7da636ea0c0a3fd7d0e395ac67
ACHRAF-by <achraf.benyahya98@gmail.com>
2023-03-08 17:50:20 +0100

    Post items and get items by Id

--- main.go
+++ main.go
@@ -15,6 +15,25 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+// album slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "To pimp A butterfly", Artist: "Kendrick Lamar", Price: 60.99},
+	{ID: "2", Title: "The Eminem Show", Artist: "Slim Shady", Price: 80.99},
+	{ID: "3", Title: "Ready to Die", Artist: "The Notorious Big", Price: 100},
+}
+
+func main() {
+	// Initialize a gin router using Default
+	router := gin.Default()
+	// Use the GET function to associate the GET HTTP method and /albums path with a handler function
+	router.GET("/albums", getItems)
+	router.POST("/albums", postItem)
+	router.GET("/albums/:id", getItemByID)
+
+	// Attach the router to http server
+	router.Run("localhost:8090")
+}
+
 // getItems function that takes a gin.Context parameter.
 // gin.Context is the most important part of Gin; it carries the request details, validates and serializes Json and more...
 // The function first argument is the HTTP status code you want to send to the client. Here we are passing statusOK from net/http package
@@ -25,19 +44,28 @@ func getItems(c *gin.Context) {
 	//c.JSON(http.StatusOK, albums)
 }
 
-func main() {
-	// Initialize a gin router using Default
-	router := gin.Default()
-	// Use the GET function to associate the GET HTTP method and /albums path with a handler function
-	router.GET("/albums", getItems)
+func getItemByID(c *gin.Context) {
+	id := c.Param("id")
 
-	// Attach the router to http server
-	router.Run("localhost:8090")
+	// Loop over the list of albums = looking for an album whose ID value matches the parameter
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
 }
 
-// album slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "To pimp A butterfly", Artist: "Kendrick Lamar", Price: 60.99},
-	{ID: "2", Title: "The Eminem Show", Artist: "Slim Shady", Price: 80.99},
-	{ID: "3", Title: "Ready to Die", Artist: "The Notorious Big", Price: 100},
+func postItem(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
